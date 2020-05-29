e999e280750ca125e20480a52114345ccc6fa78a
guser <pjharvey100@hotmail.com>
2020-05-29 17:03:53 +0200

    测试zookeeper

--- heima-leadnews-behavior/src/main/java/com/heima/behavior/service/impl/AppShowBehaviorServiceImpl.java
+++ heima-leadnews-behavior/src/main/java/com/heima/behavior/service/impl/AppShowBehaviorServiceImpl.java
@@ -1,4 +1,3 @@
-
 package com.heima.behavior.service.impl;
 
 import com.heima.behavior.service.AppShowBehaviorService;
@@ -19,6 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Service
+@SuppressWarnings("all")
 public class AppShowBehaviorServiceImpl implements AppShowBehaviorService {
 
     @Autowired
@@ -29,43 +29,46 @@ public class AppShowBehaviorServiceImpl implements AppShowBehaviorService {
 
     @Override
     public ResponseResult saveShowBehavior(ShowBehaviorDto dto) {
+        //获取用户信息，获取设备id
         ApUser user = AppThreadLocalUtils.getUser();
-
-        if (user==null&&dto.getEquipmentId()==null){
+        //根据当前的用户信息或设备id查询行为实体 ap_behavior_entry
+        if(user==null&&dto.getEquipmentId()==null){
             return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_REQUIRE);
         }
-        Long userId=null;
-        if (user!=null){
+        Long userId = null;
+        if(user!=null){
             userId=user.getId();
         }
         ApBehaviorEntry apBehaviorEntry = apBehaviorEntryMapper.selectByUserIdOrEquipemntId(userId,dto.getEquipmentId());
-
-        if (apBehaviorEntry==null){
-            return  ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        if(apBehaviorEntry==null){
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
         }
-       // List<ApArticle> articleIds=dto.getArticleIds();
-        Integer[] articleIds=new Integer[dto.getArticleIds().size()];
-        for (int i=0;i<articleIds.length;i++){
+        //获取前台传递过来的文章列表id
+//        List<ApArticle> articleIds = dto.getArticleIds();
+        Integer[] articleIds = new Integer[dto.getArticleIds().size()];
+        for(int i = 0;i<articleIds.length;i++){
             articleIds[i]=dto.getArticleIds().get(i).getId();
         }
-
-        List<ApShowBehavior> apShowBehaviors=apShowBehaviorMapper.selectListByEntryIdAndArticleIds(apBehaviorEntry.getId(),articleIds);
-
-        //List<Integer> integers = Arrays.asList(articleIds);
-        List<Integer> integers =new ArrayList<>(Arrays.asList(articleIds));
-        if (!apShowBehaviors.isEmpty()){
+        //根据行为实体id和文章列表id查询app行为表  ap_show_behavior
+        List<ApShowBehavior> apShowBehaviors = apShowBehaviorMapper.selectListByEntryIdAndArticleIds(apBehaviorEntry.getId(),articleIds);
+        //数据的过滤，需要删除表中已经存在的文章id
+        List<Integer> integers = new ArrayList<>(Arrays.asList(articleIds));
+        if(!apShowBehaviors.isEmpty()){
             apShowBehaviors.forEach(itemm->{
-                Integer articleId=itemm.getArticleId();
+                Integer articleId = itemm.getArticleId();
                 integers.remove(articleId);
             });
         }
-
-        if (!integers.isEmpty()){
+        //保存操作
+        if(!integers.isEmpty()){
             articleIds=new Integer[integers.size()];
             integers.toArray(articleIds);
+            apShowBehaviorMapper.saveShowBehavior(articleIds,apBehaviorEntry.getId());
         }
-        apShowBehaviorMapper.saveShowBehavior(articleIds,apBehaviorEntry.getId());
+
+
         return ResponseResult.okResult(0);
     }
-}
 
+
+}
