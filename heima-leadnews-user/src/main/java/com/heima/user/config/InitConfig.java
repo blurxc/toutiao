257fe224df7ecaabb118288e5ae9c361f7223bb1
guser <pjharvey100@hotmail.com>
2020-06-22 03:56:22 +0200

    创建app user项目

--- heima-leadnews-user/src/main/java/com/heima/user/config/InitConfig.java
+++
@@ -1,9 +0,0 @@
-package com.heima.login.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.heima.common.common.init")
-public class InitConfig {
-}
